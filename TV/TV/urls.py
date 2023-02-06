5fa71ff97ff6638f041010a4d3ca1666ebf40f47
Yui <98428883+antarc0y@users.noreply.github.com>
2023-02-05 21:24:54 -0700

    Update urls.py

--- TV/TV/urls.py
+++ TV/TV/urls.py
@@ -16,12 +16,33 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from TV import views
-from rest_framework.urlpatterns import format_suffix_patterns
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='TV Docs',
+        default_version='v1',
+        description='This API tracks TV shows, it respondes to GET, POST, PUT, DELETE' 
+   ),
+    public = True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shows/', views.show_list),
-    path('shows/<int:id>', views.show_detail)
+   # path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
+  #  path('shows/', views.show_list),
+    path('shows', views.show_list),
+    path('shows/<int:id>', views.show_detail),
+ #   path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
+    path('docs', schema_view.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
+#    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema_swagger'),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema_swagger'),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
