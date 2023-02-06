2968e2b9eb975312aff339153030a6a2a00ba4a3
Yui <98428883+antarc0y@users.noreply.github.com>
2023-02-05 21:25:21 -0700

    Update wsgi.py

--- TV/TV/wsgi.py
+++ TV/TV/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+# add the hellodjango project path into the sys.path
+sys.path.append('<home/ubuntu/cmput401-assignment1/TV>/TV')
+
+# add the virtualenv site-packages path to the sys.path
+#sys.path.append('<PATH_TO_VIRTUALENV>/Lib/site-packages')
+
+# poiting to the project settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TV.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TV.settings')
-
 application = get_wsgi_application()
+
+
